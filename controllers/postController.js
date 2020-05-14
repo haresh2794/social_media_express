446313f70ed3d1d678a596841891a94668aa07b7
Haresh <hareshmvc@gmail.com>
2020-05-14 07:58:08 +0530

    404 added

--- controllers/postController.js
+++ controllers/postController.js
@@ -16,10 +16,10 @@ exports.create = function(req,res){
 
 exports.viewSingle = async function(req,res){
     try{
-        let post = await Post.findSingleById(req.params.id)
-        res.render('single-post', {post: post})
+        let post = await Post.findSingleById(req.params.id) //create a post obj not with the blue print but just a call method inside
+        res.render('single-post', {post: post}) //render after the return is received
     }catch{
-        res.send("404 template will go here")
+        res.render('404')
     }
     //res.render('single-post')
 }
